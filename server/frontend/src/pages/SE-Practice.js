1d528c3ca07b10ba7ccfde1d2f62e38962449c01
Sayef <stickysayef@gmail.com>
2022-08-29 14:52:02 +1200

    first commit

--- server/frontend/src/pages/SE-Practice.js
+++ server/frontend/src/pages/SE-Practice.js
@@ -3,7 +3,7 @@ import React from "react";
 const SEPractice = () => {
     return (
       <div>
-        <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+        <h2>Select SE Practice to get evidence for the claimed benefits - Sayef</h2>
         <p>There will be a way to select an SE practice from our SEED repository here</p>
         <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
       </div>
