ba4287d0762e292e9a8e6c7bd15d89adae53c7cf
Sayef <stickysayef@gmail.com>
2022-08-29 14:58:37 +1200

    second commit

--- server/frontend/src/pages/Submit-Article.js
+++ server/frontend/src/pages/Submit-Article.js
@@ -3,7 +3,7 @@ import React from "react";
 const SubmitArticle = () =>  {
      return (
       <div>
-        <h2>Submit Article - Sayef</h2>
+        <h2>Submit Article</h2>
             <p>This will be a form to allow submitters to submit an article for possible inclusion in our SEED repo.</p>
             <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
             <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
